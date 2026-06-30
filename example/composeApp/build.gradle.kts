a6143a465a52fcab4ca4413f3b591230e1767371
IENGROUND <ericano.rhee@gmail.com>
2026-06-30 12:12:24 +0900

    Initial commit

---
+++ example/composeApp/build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    jvmToolchain(17)
+
+    androidLibrary {
+        namespace = "zone.ien.utils.example.shared"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+    
+    listOf(
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+    
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.compose.material3)
+            implementation(libs.compose.preview)
+            implementation(libs.compose.resources)
+
+
+            implementation(libs.lifecycle.viewmodel)
+            implementation(libs.lifecycle.runtime)
+
+            implementation(project(":library"))
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
