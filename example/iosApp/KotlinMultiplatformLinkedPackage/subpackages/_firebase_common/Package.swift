2c69ecb6dd77d482d9fb4cebb3dc5fa97ba9311b
IENGROUND <my@ien.zone>
2026-06-30 13:04:09 +0900

    ✨ feat(core): FirebaseApp 공통 모듈 및 플랫폼별 구현 추가
    - Android, iOS, JVM, Linux X64용 FirebaseApp 구현 추가
    - 빌드 파일 및 테스트 코드 추가로 멀티플랫폼 지원 확장
    - iOS 및 Android에서의 Firebase 연동을 위한 패키지 설정 포함

---
+++ example/iosApp/KotlinMultiplatformLinkedPackage/subpackages/_firebase_common/Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.9
+import PackageDescription
+let package = Package(
+  name: "_firebase_common",
+  platforms: [
+    .iOS("15.0")
+  ],
+  products: [
+    .library(
+      name: "_firebase_common",
+      type: .none,
+      targets: ["_firebase_common"]
+    )
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/firebase/firebase-ios-sdk.git",
+      from: "11.3.0"
+    )
+  ],
+  targets: [
+    .target(
+      name: "_firebase_common",
+      dependencies: [
+        .product(
+          name: "FirebaseCore",
+          package: "firebase-ios-sdk"
+        )
+      ]
+    )
+  ]
+)
