1102cf3fd6d15597454daab59fc5183637cbf9e7
IENGROUND <my@ien.zone>
2026-06-30 16:44:36 +0900

    ✨ feat(core): Firebase 컴포넌트 시스템 구현
    - Component, ComponentContainer, ComponentRuntime 등 핵심 클래스 추가
    - 의존성 주입 및 컴포넌트 생명주기 관리 기능 구현
    - 테스트 코드와 문서 포함하여 완전한 컴포넌트 시스템 제공

---
+++ firebase-components/build.gradle.kts
@@ -0,0 +1,94 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
+    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+group = "zone.ien.firebase"
+version = libs.versions.lib.version.name.get()
+
+kotlin {
+    jvmToolchain(17)
+
+    androidLibrary {
+        namespace = "com.google.firebase.components"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
+        withJava()
+        withHostTestBuilder {}.configure {}
+        withDeviceTestBuilder {
+            sourceSetTreeName = "test"
+        }
+
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "FirebaseComponents"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+        }
+
+        androidMain.dependencies {
+            implementation(libs.androidx.annotation)
+            implementation(libs.errorprone.annotations)
+            api(libs.firebase.annotations)
+        }
+
+        // Redirect androidMain to the existing Android SDK layout
+        val androidMain by getting {
+            kotlin.setSrcDirs(listOf("src/main/java", "src/androidMain/kotlin"))
+            resources.setSrcDirs(listOf("src/main/res"))
+        }
+
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(group.toString(), "firebase-components", version.toString())
+
+    pom {
+        name = "Firebase Components KMP"
+        description = "Kotlin Multiplatform Firebase Components DI SDK"
+        inceptionYear = "2026"
+        url = "https://github.com/ienground/firebase-kotlin-sdk"
+        licenses {
+            license {
+                name = "The Apache Software License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "repo"
+            }
+        }
+        developers {
+            developer {
+                id = "ienground"
+                name = "IEN Ground"
+            }
+        }
+        scm {
+            url = "https://github.com/ienground/firebase-kotlin-sdk.git"
+            connection = "scm:git:https://github.com/ienground/firebase-kotlin-sdk.git"
+            developerConnection = "scm:git:https://github.com/ienground/firebase-kotlin-sdk.git"
+        }
+    }
+}
