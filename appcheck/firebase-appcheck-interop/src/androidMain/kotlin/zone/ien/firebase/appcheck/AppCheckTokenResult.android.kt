f9a627d6204035e898e9eac9b2d6c947fc0abe30
IENGROUND <my@ien.zone>
2026-07-02 01:11:20 +0900

    ♻️ refactor(appcheck): AppCheckTokenResult 및 AppCheckToken 구현 개선
    - iOS 및 Android 플랫폼에서 Token과 TokenResult의 생성자 및 프로퍼티 접근 방식 통일
    - lateinit 변수 사용으로 초기화 순서 문제 해결
    - 공통 인터페이스에 대한 실제 구현 코드 수정 및 오류 처리 로직 개선
    - PlayIntegrityAppCheckProviderFactory 클래스의 생성자 변경 및 Android 전용 로직 구현
    - 테스트 코드에서 디버그 제공자 관련 메서드 시그니처 수정

--- appcheck/firebase-appcheck-interop/src/androidMain/kotlin/zone/ien/firebase/appcheck/AppCheckTokenResult.android.kt
+++ appcheck/firebase-appcheck-interop/src/androidMain/kotlin/zone/ien/firebase/appcheck/AppCheckTokenResult.android.kt
@@ -3,14 +3,12 @@ package zone.ien.firebase.appcheck
 import com.google.firebase.appcheck.AppCheckTokenResult as AndroidAppCheckTokenResult
 
 public actual class AppCheckTokenResult private actual constructor() {
-    private var androidTokenResult: AndroidAppCheckTokenResult? = null
-
-    public constructor(androidTokenResult: AndroidAppCheckTokenResult) : this() {
+    private lateinit var androidTokenResult: AndroidAppCheckTokenResult
+    internal constructor(androidTokenResult: AndroidAppCheckTokenResult) : this() {
         this.androidTokenResult = androidTokenResult
     }
-
     public actual val token: String
-        get() = androidTokenResult?.token ?: ""
+        get() = androidTokenResult.token
     public actual val error: Exception?
-        get() = androidTokenResult?.error
+        get() = androidTokenResult.error
 }
--- appcheck/firebase-appcheck-playintegrity/src/androidMain/kotlin/zone/ien/firebase/appcheck/playintegrity/PlayIntegrityAppCheckProviderFactory.android.kt
+++ appcheck/firebase-appcheck-playintegrity/src/androidMain/kotlin/zone/ien/firebase/appcheck/playintegrity/PlayIntegrityAppCheckProviderFactory.android.kt
@@ -4,13 +4,13 @@ import zone.ien.firebase.appcheck.AppCheckProviderFactory
 import com.google.firebase.appcheck.AppCheckProviderFactory as AndroidAppCheckProviderFactory
 import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory as AndroidPlayIntegrityAppCheckProviderFactory
 
-public actual class PlayIntegrityAppCheckProviderFactory private constructor(
-    private val androidPlayIntegrityFactory: AndroidPlayIntegrityAppCheckProviderFactory
-) : AppCheckProviderFactory {
-
+public actual class PlayIntegrityAppCheckProviderFactory private actual constructor() : AppCheckProviderFactory {
+    private lateinit var androidPlayIntegrityFactory: AndroidPlayIntegrityAppCheckProviderFactory
+    internal constructor(androidPlayIntegrityFactory: AndroidPlayIntegrityAppCheckProviderFactory) : this() {
+        this.androidPlayIntegrityFactory = androidPlayIntegrityFactory
+    }
     override val androidFactory: AndroidAppCheckProviderFactory
         get() = androidPlayIntegrityFactory
-
     public actual companion object {
         public actual fun getInstance(): PlayIntegrityAppCheckProviderFactory {
             return PlayIntegrityAppCheckProviderFactory(AndroidPlayIntegrityAppCheckProviderFactory.getInstance())
