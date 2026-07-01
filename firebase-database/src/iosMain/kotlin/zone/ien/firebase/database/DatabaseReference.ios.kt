9e928244f91cd797cc19d08d482c24e6b1300ca3
IENGROUND <my@ien.zone>
2026-07-01 17:20:25 +0900

    fix(core): DatabaseReference iOS suspend 함수 구현 개선
    - setValue, removeValue, updateChildren 함수에 suspendCancellableCoroutine 사용으로 코루틴 취소 처리 개선
    - iOS에서의 값 변환 로직 추가하여 Map 타입 정상 처리
    - 코루틴 활성 상태 확인 후 결과 반환으로 메모리 누수 방지

--- firebase-database/src/iosMain/kotlin/zone/ien/firebase/database/DatabaseReference.ios.kt
+++ firebase-database/src/iosMain/kotlin/zone/ien/firebase/database/DatabaseReference.ios.kt
@@ -1,10 +1,10 @@
 package zone.ien.firebase.database
 
 import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.suspendCancellableCoroutine
 import swiftPMImport.zone.ien.firebase.firebase.database.FIRDatabaseReference
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
-import kotlin.coroutines.suspendCoroutine
 
 @OptIn(ExperimentalForeignApi::class)
 public actual class DatabaseReference(private val iosReference: FIRDatabaseReference) {
@@ -20,33 +20,39 @@ public actual class DatabaseReference(private val iosReference: FIRDatabaseRefer
         return DatabaseReference(iosReference.childByAutoId())
     }
 
-    public actual suspend fun setValue(value: Any?): Unit = suspendCoroutine { cont ->
-        iosReference.setValue(value) { error, _ ->
-            if (error != null) {
-                cont.resumeWithException(DatabaseException(error.localizedDescription, null))
-            } else {
-                cont.resume(Unit)
+    public actual suspend fun setValue(value: Any?): Unit = suspendCancellableCoroutine { cont ->
+        val iosValue = if (value is Map<*, *>) value as Map<Any?, *> else value
+        iosReference.setValue(iosValue) { error, _ ->
+            if (cont.isActive) {
+                if (error != null) {
+                    cont.resumeWithException(DatabaseException(error.localizedDescription, null))
+                } else {
+                    cont.resume(Unit)
+                }
             }
         }
     }
 
-    public actual suspend fun removeValue(): Unit = suspendCoroutine { cont ->
+    public actual suspend fun removeValue(): Unit = suspendCancellableCoroutine { cont ->
         iosReference.removeValueWithCompletionBlock { error, _ ->
-            if (error != null) {
-                cont.resumeWithException(DatabaseException(error.localizedDescription, null))
-            } else {
-                cont.resume(Unit)
+            if (cont.isActive) {
+                if (error != null) {
+                    cont.resumeWithException(DatabaseException(error.localizedDescription, null))
+                } else {
+                    cont.resume(Unit)
+                }
             }
         }
     }
 
-    public actual suspend fun updateChildren(update: Map<String, Any?>): Unit = suspendCoroutine { cont ->
-        val iosMap = update.mapKeys { it.key as Any? }
-        iosReference.updateChildValues(iosMap) { error, _ ->
-            if (error != null) {
-                cont.resumeWithException(DatabaseException(error.localizedDescription, null))
-            } else {
-                cont.resume(Unit)
+    public actual suspend fun updateChildren(update: Map<String, Any?>): Unit = suspendCancellableCoroutine { cont ->
+        iosReference.updateChildValues(update as Map<Any?, *>) { error, _ ->
+            if (cont.isActive) {
+                if (error != null) {
+                    cont.resumeWithException(DatabaseException(error.localizedDescription, null))
+                } else {
+                    cont.resume(Unit)
+                }
             }
         }
     }
