d1decd39cb867a29dd692bcdef58ebb20413eaa1
IENGROUND <my@ien.zone>
2026-07-02 12:34:37 +0900

    ✨ feat(core): App.kt 파일의 Composable 함수 구조 개선
    - App.kt에 Composable 함수로 변경하여 상태 관리 및 네비게이션 구현
    - M3RootWrapper를 사용한 UI 래핑 및 Navigation3 State Framework 적용
    - FirebasePlatformContext를 통해 플랫폼 컨텍스트 주입 구조 개선

--- example/composeApp/src/commonMain/kotlin/zone/ien/firebase/example/App.kt
+++ example/composeApp/src/commonMain/kotlin/zone/ien/firebase/example/App.kt
@@ -2,8 +2,6 @@ package zone.ien.firebase.example
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import zone.ien.firebase.FirebasePlatformContext
@@ -11,20 +9,24 @@ import zone.ien.firebase.example.ui.navigation.ScreenNavigationGraph
 import zone.ien.firebase.example.ui.navigation.ScreenRoute
 import zone.ien.firebase.example.ui.theme.AppTheme
 import zone.ien.utils.navigation.rememberNavBackStack
+import zone.ien.utils.ui.wrapper.M3RootWrapper
 
 @Composable
 fun App(context: FirebasePlatformContext) {
     // Explicit Backstack key-based Navigation3 State Framework
     val backStack = rememberNavBackStack<ScreenRoute>(ScreenRoute.Home)
-    AppTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            ScreenNavigationGraph(
-                context = context,
-                backStack = backStack
-            )
+
+    M3RootWrapper {
+        AppTheme {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                ScreenNavigationGraph(
+                    context = context,
+                    backStack = backStack
+                )
+            }
         }
     }
 }
