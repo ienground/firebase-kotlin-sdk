7d4de5c8363e585689ae66490663a0d64e35c30b
IENGROUND <my@ien.zone>
2026-07-05 08:47:48 +0900

    ♻️ refactor(core): DataDecoder 인터페이스 리팩토링
    - DataDecoder.kt 파일에서 decode 함수의 제네릭 타입 선언 및 구현 변경
    - build.gradle.kts 파일에서 테스트 의존성 추가 및 구조 정리
    - 코어 모듈의 디코더 로직 개선 및 코드 구조 간소화

--- encoders/firebase-decoders-json/src/commonMain/kotlin/zone/ien/firebase/decoders/json/DataDecoder.kt
+++ encoders/firebase-decoders-json/src/commonMain/kotlin/zone/ien/firebase/decoders/json/DataDecoder.kt
@@ -5,6 +5,9 @@ import kotlin.reflect.KClass
 public interface DataDecoder {
     public fun <T : Any> decode(json: String, clazz: KClass<T>): T
 }
+public inline fun <reified T : Any> DataDecoder.decode(json: String): T {
+    return decode(json, T::class)
+}
 
 public interface ObjectDecoder<T> {
     public fun decode(source: Map<String, Any?>): T
