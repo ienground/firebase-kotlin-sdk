2d5a8056e77a91ee145a52f6d9ed4b8a5664b835
IENGROUND <my@ien.zone>
2026-07-06 16:07:41 +0900

    🐛 fix(components): 동적 로딩 등록자 actual 정합성 수정
    - DynamicLoadingRegistrar expect를 Android SDK 클래스 구조에 맞게 class로 변경
    
    - iOS actual 구현을 class로 맞추고 빈 component 목록 반환
    
    - dynamic-module-support Android/iOS 컴파일 및 host 테스트 통과 확인

--- firebase-components/firebase-dynamic-module-support/src/iosMain/kotlin/zone/ien/firebase/dynamicloading/DynamicLoadingRegistrar.ios.kt
+++ firebase-components/firebase-dynamic-module-support/src/iosMain/kotlin/zone/ien/firebase/dynamicloading/DynamicLoadingRegistrar.ios.kt
@@ -1,5 +1,8 @@
 package zone.ien.firebase.dynamicloading
 
+import zone.ien.firebase.components.Component
 import zone.ien.firebase.components.ComponentRegistrar
 
-actual interface DynamicLoadingRegistrar : ComponentRegistrar
+actual class DynamicLoadingRegistrar : ComponentRegistrar {
+    actual override fun getComponents(): List<Component<*>> = emptyList()
+}
