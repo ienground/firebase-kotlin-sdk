0421e7b43570fccfe73eebcdfb534adde4d6e00d
IENGROUND <my@ien.zone>
2026-07-06 23:17:57 +0900

    fix(encoders): Resolve FieldDescriptor properties annotation proxy mapping failure on Kotlin Native/iOS

--- encoders/firebase-encoders/src/androidMain/kotlin/zone/ien/firebase/encoders/AnnotationKey.android.kt
+++ encoders/firebase-encoders/src/androidMain/kotlin/zone/ien/firebase/encoders/AnnotationKey.android.kt
@@ -3,7 +3,7 @@ package zone.ien.firebase.encoders
 import kotlin.jvm.kotlin
 import kotlin.reflect.KClass
 
-internal actual fun <T : Annotation> T.annotationKey(): KClass<out Annotation> {
+internal actual fun <T : Annotation> T.annotationKey(): String {
     @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-    return (this as java.lang.annotation.Annotation).annotationType().kotlin
+    return (this as java.lang.annotation.Annotation).annotationType().name
 }
--- encoders/firebase-encoders/src/iosMain/kotlin/zone/ien/firebase/encoders/AnnotationKey.ios.kt
+++ encoders/firebase-encoders/src/iosMain/kotlin/zone/ien/firebase/encoders/AnnotationKey.ios.kt
@@ -2,6 +2,14 @@ package zone.ien.firebase.encoders
 
 import kotlin.reflect.KClass
 
-internal actual fun <T : Annotation> T.annotationKey(): KClass<out Annotation> {
-    return this::class
+internal actual fun <T : Annotation> T.annotationKey(): String {
+    val qualifiedName = this::class.qualifiedName ?: ""
+    return if (qualifiedName.contains("annotationImpl$")) {
+        val pkg = qualifiedName.substringBefore("annotationImpl$")
+        val rest = qualifiedName.substringAfter("annotationImpl$").substringBeforeLast("$")
+        val simpleAnnName = rest.substringAfterLast("_")
+        pkg + simpleAnnName
+    } else {
+        qualifiedName
+    }
 }
